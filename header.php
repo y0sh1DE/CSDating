7e78e5dd1d7705789f1dabfbe434c1f1c486ec76
Yoshi <32636689+y0sh1DE@users.noreply.github.com>
2019-07-30 00:29:08 +0200

    Update header.php

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="icon" href="favicon.ico" type="image/x-icon">
-        <title>CSDating V0.3.2</title>
+        <title>CSDating V0.3.3</title>
     </head>
     <body>
     <div class="container">
