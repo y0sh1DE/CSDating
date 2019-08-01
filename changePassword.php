bf8f28d949d3740b0463bc9cce4796d22f2771f4
Yoshi <32636689+y0sh1DE@users.noreply.github.com>
2019-08-01 19:13:01 +0200

    Solved Issue #4
    Fixed this Issue on the client side.

--- changePassword.php
+++ changePassword.php
@@ -6,9 +6,9 @@
     <div class="container">
         <div class="form-group">
             <form action="includes/changepassword.inc.php" name="frmChangePassword" id="frmChangePassword">
-                <div class="form-group"><input type="password" name="tbxOldPassword" id="tbxOldPassword" placeholder="Old Password"/></div>
-                <div class="form-group"><input type="password" name="tbxNewPassword" id="tbxNewPassword" placeholder="New Password"/></div>
-                <div class="form-group"><input type="password" name="tbxNewPasswordRepeat" id="tbxNewPasswordRepeat" placeholder="Repeat new Password"/></div>
+                <div class="form-group"><input type="password" name="tbxOldPassword" id="tbxOldPassword" placeholder="Old Password" required/></div>
+                <div class="form-group"><input type="password" name="tbxNewPassword" id="tbxNewPassword" placeholder="New Password" required/></div>
+                <div class="form-group"><input type="password" name="tbxNewPasswordRepeat" id="tbxNewPasswordRepeat" placeholder="Repeat new Password" required/></div>
                 <div class="form-group"><button type="submit" id="btnSubmit" name="btnSubmit" class="btn btn-primary">Change Password</button>
                 <input type="hidden" name="username" value="<?php echo $_GET['username']; ?>"/>
                 <button type="reset" id="btnReset" name="btnReset" class="btn btn-secondary">Reset</button></div>
