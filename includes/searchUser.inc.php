c34576af0dc17a409d24f9ddd069f7451aa1c1f9
y0sh1DE <chainzzockt@gmail.com>
2019-10-19 19:53:47 +0200

    Added Codebase for #21

---
+++ includes/searchUser.inc.php
@@ -0,0 +1,18 @@
+<?php
+  $input = $_POST["tbxSearchUser"];
+  if(is_numeric($input))
+  {
+    header("Location: ../profile.php?uid=" + $input);
+    exit();
+  }
+  else
+  {
+    require_once "dbh.inc.php";
+    $sql = sprintf("SELECT uID FROM tbluser WHERE uName = '%s'", $input);
+    $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+    $row = mysqli_fetch_row($result);
+    $url = sprintf("profile.php?uid=%s", $row[0]);
+    header("Location: ../" + $url);
+    exit();
+  }
+?>
