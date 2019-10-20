73300c89f39b049b943730bb50268f442c8d226f
y0sh1DE <chainzzockt@gmail.com>
2019-10-20 17:12:40 +0200

    Added profile link to whoIsOnline list

--- whoIsOnline.php
+++ whoIsOnline.php
@@ -16,7 +16,7 @@
                 <tbody>
                     <?php
                         require_once "includes/dbh.inc.php";
-                        $sql = "SELECT uName, uLevel, uLastAddress FROM tbluser WHERE uLoggedIn = 1 AND uLevel >= 1";
+                        $sql = "SELECT uName, uLevel, uLastAddress, uID FROM tbluser WHERE uLoggedIn = 1 AND uLevel >= 1";
                         $result = mysqli_query($conn, $sql);
                         while ($row = mysqli_fetch_assoc($result))
                         {
@@ -25,12 +25,12 @@
                             $out = sprintf("
                             <tr>
                                 <td>
-                                    %s
+                                    <a href="profile.php?uid=%s>%s</a>
                                 </td>
                                 <td>
                                     %s
                                 </td>
-                            ", $row['uName'], $level);
+                            ",$row['uID'] ,$row['uName'], $level);
 
                             if($_SESSION['uLevel'] == 2) $out .= sprintf("<td>%s</td>", $row['uLastAddress']);
 
