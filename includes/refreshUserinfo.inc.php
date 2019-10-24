71698b422c903132d81a07801f57d660d4a0d01c
y0sh1DE <chainzzockt@gmail.com>
2019-10-24 11:50:50 +0200

    Tried to fix #13

--- includes/refreshUserinfo.inc.php
+++ includes/refreshUserinfo.inc.php
@@ -1,5 +1,9 @@
 <?php
     require_once "dbh.inc.php";
+    require_once "config.inc.php";
+    session_start();
+    setcookie(session_name(),session_id(),time() + $GLOBALS['SESSION_LIFETIME']); 
+
     $sql = sprintf("SELECT * FROM tbluser WHERE uName = '%s'", $_SESSION['uName']);
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($result);
